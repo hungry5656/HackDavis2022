7f8d079b90bc55567123b2502b5c770ce5018afb
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-16 17:28:02 -0700

    create daily check

---
+++ backend/models/dailyCheck.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import updateDB from './updateDB';
+
+function dailyCheck(){
+    
+    let email;
+    let phoneNum;
+    let lastApptDate;
+    let nextApptDate;
+    let doseType;
+    let doseNum;
+    let petType;
+
+}
+
+export default dailyCheck;
