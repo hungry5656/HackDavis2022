58e6609169a34f7a4b98806f56cc038c8a472398
NathanUCD <contactnathanldc@gmail.com>
2022-04-17 11:43:47 -0700

    last min updates

--- backend/scripts/SMS.js
+++ backend/scripts/SMS.js
@@ -2,17 +2,18 @@ import schedule from 'node-schedule';
 
 const accountSid = 'AC613a84670625eceacc958d8adace6c12';
 const authToken = '7add8275886c867268f851abc640c94b';
-const client = require('twilio')(accountSid, authToken);
-
-
+//const client = require('twilio')(accountSid, authToken);
+import twilio from 'twilio'; 
+const client = twilio(accountSid, authToken); 
 // let nickName, pet_name, vaccination_type, date, phone_number;
 // nickName = 'Wenhao'
 // pet_name = 'pepper'
 // vaccination_type = 'Rabies'
 // date = '2022/4/16';
 // phone_number = '4258665228'
+let SMSsender = {}; 
 
-SMSsender.sendSMS()= (sendTo, msgContent) => {
+SMSsender.sendSMS = (sendTo, msgContent) => {
     let data = {
         content: msgContent
     }
--- hd2022/src/App.js
+++ hd2022/src/App.js
@@ -21,11 +21,11 @@ function App() {
             <h1>Please Enter your Vaccine info in the next page</h1>
             <h2>Select the vaccine type in the drop down menu</h2>
             <br></br>
-            <Button onClick={() => document.getElementById("animalChoice").scrollIntoView()}> Get Started </Button>
+            <Button onClick={() => document.getElementById("animalDetails").scrollIntoView()}> Get Started </Button>
           </div>
         </section>
         
-        <div id= "animalDetails"></div>
+        <div id= "contactDetails"></div>
         <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
           <div class="container text-center text-md-left" data-aos="fade-up">
             <h1>Please enter your information below.</h1>
