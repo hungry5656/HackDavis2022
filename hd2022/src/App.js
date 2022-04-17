29e7c361411d5365c3693371d00de1225f0b04c9
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-17 02:41:09 -0700

    update files in frontend

--- hd2022/src/App.js
+++ hd2022/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -6,14 +5,13 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <title>VetVax</title>
-        <img src={logo} className="App-logo" alt="logo" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
-      </header>
-      <body className = "">
-
-      </body>
+      <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+        <div class="container text-center text-md-left" data-aos="fade-up">
+          <h1>Please Enter your Vaccine info in the next page</h1>
+          <h2>Select the vaccine type in the drop down menu</h2>
+          {/* <a href="#start" class="btn-get-started scrollto">Get Started</a> */}
+        </div>
+      </section>
     </div>
   );
 }
