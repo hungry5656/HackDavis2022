37631dd139766f89f790170701571a85c8bfbdcc
NathanUCD <contactnathanldc@gmail.com>
2022-04-17 11:19:11 -0700

    updated react app

--- hd2022/src/App.js
+++ hd2022/src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import styled from 'styled-components'; 
+import { Dropdown } from 'react-bootstrap'; 
+import { useState } from 'react'; 
 
 function App() {
   // JavaScript goes here 
@@ -9,12 +12,8 @@ function App() {
   padding: 10px 60px;
   border-radius: 5px;
   margin: 10px 0px;
-  cursor: pointer;
-`;
-  function helloW(){
-    alert("Hello!");
-  }
-
+  cursor: pointer;`;
+  
   return (
     <div className="App">
       <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
@@ -22,17 +21,34 @@ function App() {
           <h1>Please Enter your Vaccine info in the next page</h1>
           <h2>Select the vaccine type in the drop down menu</h2>
           <br></br>
-          <Button onClick="helloW()">
-          Next
-          </Button>
-          {/* <a href="#start" class="btn-get-started scrollto">Get Started</a> */}
+          <Button onClick={() => document.getElementById("animalChoice").scrollIntoView()}> Get Started </Button>
         </div>
       </section>
-      <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+
+      <section id="animalChoice" class="d-flex flex-column justify-content-center align-items-center">
         <div class="container text-center text-md-left" data-aos="fade-up">
-          <h1>Please Enter your Vaccine info in the next page</h1>
-          <h2>Select the vaccine type in the drop down menu</h2>
-          {/* <a href="#start" class="btn-get-started scrollto">Get Started</a> */}
+          <h1>Please enter your information below.</h1>
+          <Dropdown>
+            <Dropdown.Toggle>Animal</Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => document.getElementById("dogSection").scrollIntoView()}> Dawg </Dropdown.Item>
+              <Dropdown.Item onClick={() => document.getElementById("catSection").scrollIntoView()}> Cate </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </section>
+
+      <section id="dogSection" class="d-flex flex-column justify-content-center align-items-center">
+        <div class="container text-center text-md-left" data-aos="fade-up">
+          <h1>Please enter your information below.</h1>
+          
+        </div>
+      </section>
+
+      <section id="catSection" class="d-flex flex-column justify-content-center align-items-center">
+        <div class="container text-center text-md-left" data-aos="fade-up">
+          <h1>Please enter your information below.</h1>
+          
         </div>
       </section>
     </div>
